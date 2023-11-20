e67799222688a8a163fdc6263448e60635180e01
Misikir Abate <Misikir.A@MACROGC.LOCAL>
2023-11-20 14:42:21 +0300

    update the home page text

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Welome to the world of nextjs</h1>
+      <h1>Welome to the world of next js</h1>
       <Link href="/user">user</Link>
     </main>
   );
